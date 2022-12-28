234c96c6701f93d6d79533a2bb6f0a1ab91c7ece
Biswajit Debnath <biswajit.basanti@gmail.com>
2022-12-28 17:36:27 +0530

    db updated

--- db.js
+++ db.js
@@ -3,15 +3,18 @@ require('dotenv').config();
 
 const connectToMongo=async()=>{
     try {
-        // if (process.env.DB == null) {
-        //     mongoURI = "mongodb://127.0.0.1:27017";
-        // }
-        // // Use this instead if running the dockerfile
-        // else {
-        //     mongoURI = "mongodb://mongodb:27017"
-        // }
-        const conn = await mongoose.connect(`mongodb+srv://${process.env.DATABASE_ID}:${process.env.DATABASE_PASS}@cluster0.4m23f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, { useNewUrlParser: true});
-        console.log("successfully connected with mongo atlas");
+        if (process.env.DATABASE_ID == null) {
+            const conn = await mongoose.connect('mongodb://127.0.0.1:27017/meranote')
+                .then(() => { console.log("successfully connected with mongo"); })
+                .catch((err)=>{console.log(err)});
+        }
+        else {
+            const conn = await mongoose.connect(`mongodb+srv://${process.env.DATABASE_ID}:${process.env.DATABASE_PASS}@cluster0.4m23f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, { useNewUrlParser: true})
+                .then(() => { console.log("successfully connected with mongo atlas")})
+                .catch((err) => { console.log(err) });
+        }
+        
+        
     } catch (error) {
         console.log(error);
     }
